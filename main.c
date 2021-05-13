e006fa1b19adc9db4623440a779cfb259e16c5a7
glennp1 <61995051+glennp1@users.noreply.github.com>
2021-05-13 12:32:56 +1000

    Created main.c and added skeleton for phase 1

---
+++ main.c
@@ -0,0 +1,35 @@
+//
+// Created by Glenn Phillips on 10/5/21.
+//
+
+
+
+typedef struct {
+    // todo add the input arguments
+} input_arguments_t;
+
+
+int main(int argc, char *argv[]) {
+
+    // read_in_packet(char *argv)
+
+
+    return 0;
+}
+
+
+// read in a packet from std in
+
+// input_arguments_t handle_input(int num_arguments, char *arguments[]) {
+//
+//
+// }
+
+
+// parse the packet
+
+
+
+// output the log entry to the file ./dns_svr.log
+
+
