16c19fcc22b030fb7b1c4cf698140a0619c773fc
glennp1 <61995051+glennp1@users.noreply.github.com>
2021-05-17 22:29:41 +1000

    added a template header and template source file

---
+++ template.c
@@ -0,0 +1,19 @@
+//
+// Created by Glenn Phillips on 17/5/21.
+//
+
+// --- System Libraries ---
+
+// --- Project Libraries ---
+#include "template.h"
+
+// --- Constant Definitions ---
+
+// --- Type Definitions ---
+
+// --- Helper Function Prototypes ---
+
+// --- Function Implementations ---
+
+// --- Helper Function Implementations ---
+
---
+++ template.h
@@ -0,0 +1,18 @@
+//
+// Created by Glenn Phillips on 17/5/21.
+//
+
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+// --- System Libraries ---
+
+// --- Project Libraries ---
+
+// --- Constant Definitions ---
+
+// --- Type Definitions ---
+
+// --- Function Prototypes ---
+
+#endif //TEMPLATE_H
